cc953203db328dd7b81c79f01664ca039337476c
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-11-05 14:07:37 -0500

    WIP: redux store actions-reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
+
 import App from './App';
+import './index.css';
+
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
