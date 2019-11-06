324f2dd9ba3f5a9687c99420e1bae131f77dc58f
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-11-06 08:27:12 -0500

    login page init structure

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+import logo from '../logo.png';
 // import "./Home.css";
 
-export default function Login() {
-  return (
-    <div className="Login">
-      <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  render () {
+    return (
+      <div className="Login">
+        <div className="d-flex flex-column align-items-center justify-content-center">
+          <div className="flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+            <img src={logo} className="logo" alt="logo" />
+            <div className="title">Game Scoreboard</div>
+          </div>
+          <div className="footer mt-auto">Developed by Saludzinho™</div>
+        </div>
+        <div className="lander">
+          <h1>LOGIN</h1>
+          <p>A simple note taking app</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default connect(null, null)(Login);
