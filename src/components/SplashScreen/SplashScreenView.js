f768495a934184c4d1e1e311bdbe51b9eee2618f
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-11-07 14:55:42 -0500

    removed unused props

--- src/components/SplashScreen/SplashScreenView.js
+++ src/components/SplashScreen/SplashScreenView.js
@@ -1,10 +1,9 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import logo from '../../assets/logo.png';
 import './SplashScreen.css';
 
-const SplashScreen = (props) => (
+const SplashScreen = () => (
   <div className="splash-screen d-flex flex-column align-items-center justify-content-center">
     <div className="flex-grow-1 d-flex flex-column align-items-center justify-content-center">
       <img src={logo} className="logo" alt="logo" />
@@ -13,8 +12,5 @@ const SplashScreen = (props) => (
     <div className="footer mt-auto">Developed by Saludzinho™</div>
   </div>
 );
-
-SplashScreen.propTypes = {
-};
 
 export default SplashScreen
