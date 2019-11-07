75c58c286fb820d6b6ef90cb988e45c6b63e258c
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-11-07 14:54:55 -0500

    added comments to explain purpose of wrapper component

--- src/components/SplashScreen/withSplashScreen.js
+++ src/components/SplashScreen/withSplashScreen.js
@@ -4,6 +4,12 @@ import { connect } from 'react-redux';
 import { authActions } from '../../redux/actions'
 import SplashScreen from './SplashScreenContainer'
 
+/*
+ withSplashScreen is a wrapper component that is called
+ as a function with the component to be "wrapped"
+ NOTE: it is intended to wrap the `App.js` component as a landing page
+ for the application to pull together all the necessary data
+ */
 function withSplashScreen(WrappedComponent) {
   return connect(null, mapDispatchToProps)(SplashScreen(WrappedComponent));
 }
