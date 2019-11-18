513d255845dba18f521bd5778ca533edc9c1c3e9
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-11-18 14:50:31 -0500

    removed fontawesome dependency

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faEnvelope,
-  faLock,
-  faSpinner,
-  faStar,
-  faUser
-} from '@fortawesome/free-solid-svg-icons'
 
 import Routes from "../../routes/Routes";
 import withSplashScreen from '../SplashScreen';
 import Modal from '../Modal';
-
-library.add(faEnvelope, faLock, faSpinner, faStar, faUser);
 
 class App extends Component {
   render() {
