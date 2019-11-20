2ea46a4f7bbf24c3f7849045744f64252d8ab82c
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-11-20 16:58:09 -0500

    avatar ui fixes for proper circle and source path

--- src/components/Dashboard/HeaderBar/HeaderBar.js
+++ src/components/Dashboard/HeaderBar/HeaderBar.js
@@ -20,7 +20,7 @@ const HeaderBar = (props) => {
         flex flex-row flex-no-wrap items-center
         w-full md:w-4/5
       ">
-        <img className="h-16 rounded-full hidden md:block mr-6" src={avatarSource} alt="avatar" />
+        <img className="h-16 w-16 rounded-full hidden md:block mr-6" src={avatarSource} alt="avatar" />
         <div className="flex flex-col">
           <h1 className="text-lg">Welcome, {props.player.first_name} {props.player.last_name}</h1>
           <div className="text-xs">
