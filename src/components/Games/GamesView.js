3da598fa9ae30dd8979750028ffbb76e8d665f70
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-12-11 15:49:43 -0500

    added breadcrumbs to views

--- src/components/Games/GamesView.js
+++ src/components/Games/GamesView.js
@@ -2,22 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import GameCard from "./GameCard"
+import Breadcrumb from '../Breadcrumb';
 
-const Games = (props) => (
-  <div className="Games
-    h-full w-full
-    p-3 md:px-16 md:py-8">
-    <h1 className="pb-3 text-xl font-medium">Games</h1>
+const Games = (props) => {
+  const crumbs = [
+    ['Home', '/'],
+    ['Games', '/games']
+  ];
+
+  return (
     <div className="
-      flex flex-row flex-wrap
+      flex flex-col
+      p-3 md:px-16 md:py-8
     ">
-      {props.games.map((game, key) =>
-        <GameCard key={key}
-          game={game} />
-      )}
+      <Breadcrumb crumbs={crumbs} />
+      <div className="Games
+        h-full w-full">
+        <div className="
+          flex flex-row flex-wrap
+        ">
+          {props.games.map((game, key) =>
+            <GameCard key={key}
+              game={game} />
+          )}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Games.propTypes = {
   games: PropTypes.array.isRequired
--- src/components/Players/PlayersView.js
+++ src/components/Players/PlayersView.js
@@ -1,23 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import PlayerCard from './PlayerCard'
+import PlayerCard from './PlayerCard';
+import Breadcrumb from '../Breadcrumb';
 
-const Players = (props) => (
-  <div className="Players
-    h-full w-full
-    p-3 md:px-16 md:py-8">
-    <h1 className="pb-3 text-xl font-medium">Players</h1>
+const Players = (props) => {
+  const crumbs = [
+    ['Home', '/'],
+    ['Players', '/players']
+  ];
+
+  return (
     <div className="
-      flex flex-row flex-wrap
+      flex flex-col
+      p-3 md:px-16 md:py-8
     ">
-      {props.players.map((player, key) =>
-        <PlayerCard key={key}
-          player={player} />
-      )}
+      <Breadcrumb crumbs={crumbs} />
+      <div className="Players
+        h-full w-full">
+        <div className="
+          flex flex-row flex-wrap
+        ">
+          {props.players.map((player, key) =>
+            <PlayerCard key={key}
+              player={player} />
+          )}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Players.propTypes = {
   players: PropTypes.array.isRequired
